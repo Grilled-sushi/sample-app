14ab40d29b5153800204104883f4a044684f45a9
Kengo <kengo_pika@yahoo.co.jp>
2023-03-08 18:54:26 +0900

    13まで

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,46 @@
 const express = require('express');
 const router = express.Router();
 
-let todos = [];
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '164898leyhLeyh164898',
+  database: 'todo_app'
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {
-    title: 'Todo App',
-    todos: todos
+  connection.query(
+    `select * from tasks;`,
+    (error, results) => {
+      console.log(error);
+      console.log(results);
+      res.render('index', {
+        title: 'Todo App',
+        todos: results
+    }
+  )
   });
 });
 
 router.post('/', function(req, res, next) {
+  connection.connect((err) => {
+    if (err) {
+      console.log('error connecting: ' + err.stack);
+      return;
+    }
+    console.log('success');
+  })
   const todo = req.body.add;
-  todos.push(todo);
-  res.redirect('/');
+  connection.query(
+    `insert into tasks (user_id, content) values (1, '${todo}');`,
+    (error, results) => {
+      console.log(error);
+      res.redirect('/');
+    }
+  )
 })
 
 module.exports = router;
